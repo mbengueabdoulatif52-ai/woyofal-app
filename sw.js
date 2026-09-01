04d68bccd8a6fb31944458ff8930cb531037bda8
mbengueabdoulatif52-ai <mbengueabdoulatif52@gmail.com>
2026-09-01 21:03:15 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "woyofal-v2";
-const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+const CACHE_NAME = "woyofal-v3";
+const CORE_ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)));
